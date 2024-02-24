74d5523c2f964a0fdd81dff96b6993230e201f77
Neon <neon@saahild.com>
2024-02-24 17:25:48 -0500

    eslint * prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-console.log("Coming soon...")
+console.log('Coming soon...')
 
 // incase someone trys to import it
 // module.exports = {}
 module.exports = {
-    version: require('../package.json').version,    
+  version: require('../package.json').version
 }
